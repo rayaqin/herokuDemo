f60c2c27ad36dcc6cb8065f774d0b7e887891660
rayaqin <rayaqin@gmail.com>
2020-10-07 00:35:11 +0200

    Update DemoApplication.java
    second endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,22 @@ public class DemoApplication {
 		return "Hello World!";
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping("/numbers")
+	@ResponseBody
+	String returnNumbers() {
+		return String.valueOf(getRandomNumbers(100, 10));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	private static int[] getRandomNumbers(int a, int b) {
+		int[] result = new int[b];
+		for(int i=0; i<b; i++){
+			result[i] = a + (int) ((1 - a) * Math.random());
+		}
+		return result;
 	}
 }
